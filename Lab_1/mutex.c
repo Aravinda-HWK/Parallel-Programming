593689f50522cffcc8555e955797196ea624654d
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-07-28 20:54:08 +0530

    Update mutex file

--- Lab_1/mutex.c
+++ Lab_1/mutex.c
@@ -4,37 +4,37 @@
 #include <time.h>
 
 // Define the structure for a list node
-struct list_node_s
-{
+struct list_node_s {
     int data;
     struct list_node_s *next;
 };
-void print_list(struct list_node_s *head)
-{
-    struct list_node_s *current = head;
-    while (current != NULL)
-    {
-        printf("%d -> ", current->data);
-        current = current->next;
-    }
-    printf("NULL\n");
-}
 
 // Mutex for the linked list
-pthread_mutex_t list_mutex, list_mutex1;
+pthread_mutex_t list_mutex;
 int *opr_values;
-int count, count_m, count_i, count_d = 0;
 int m_member, m_insert, m_delete;
 int n;
 int m;
 int thread_count;
 
+// Structure to hold thread arguments
+typedef struct {
+    struct list_node_s **head_pp;
+    long rank;
+} thread_data_t;
+
+// Function prototypes
+void print_list(struct list_node_s *head);
+int Member(int value, struct list_node_s *head_p);
+int InsertWithoutParellel(int value, struct list_node_s **head_pp);
+int Insert(int value, struct list_node_s **head_pp);
+int Delete(int value, struct list_node_s **head_pp);
+void *ThreadFunction(void *arg);
+
 // Function to check if a value is a member of the linked list
-int Member(int value, struct list_node_s *head_p)
-{
+int Member(int value, struct list_node_s *head_p) {
     struct list_node_s *curr_p = head_p;
     pthread_mutex_lock(&list_mutex);
-    count_m++;
     // Traverse the list to find the value
     while (curr_p != NULL && curr_p->data < value)
         curr_p = curr_p->next;
@@ -45,84 +45,66 @@ int Member(int value, struct list_node_s *head_p)
     return found;
 }
 
-// Function to insert a new node into the list
-int InsertWithoutParellel(int value, struct list_node_s **head_pp)
-{
-
+// Function to insert a new node into the list without parallelism
+int InsertWithoutParellel(int value, struct list_node_s **head_pp) {
     struct list_node_s *curr_p = *head_pp;
     struct list_node_s *pred_p = NULL;
     struct list_node_s *temp_p;
 
     // Traverse the list to find the correct position to insert
-    while (curr_p != NULL && curr_p->data < value)
-    {
+    while (curr_p != NULL && curr_p->data < value) {
         pred_p = curr_p;
         curr_p = curr_p->next;
     }
 
     // If value is not already in the list, insert the new node
-    if (curr_p == NULL || curr_p->data > value)
-    {
+    if (curr_p == NULL || curr_p->data > value) {
         temp_p = malloc(sizeof(struct list_node_s));
         temp_p->data = value;
         temp_p->next = curr_p;
 
-        if (pred_p == NULL)
-        {
+        if (pred_p == NULL) {
             // New first node
             *head_pp = temp_p;
-        }
-        else
-        {
+        } else {
             pred_p->next = temp_p;
         }
         return 1;
-    }
-    else
-    {
+    } else {
         // Value already in list
         return 0;
     }
 }
 
 // Function to insert a new node into the list
-int Insert(int value, struct list_node_s **head_pp)
-{
+int Insert(int value, struct list_node_s **head_pp) {
     pthread_mutex_lock(&list_mutex);
-    count_i++;
 
     struct list_node_s *curr_p = *head_pp;
     struct list_node_s *pred_p = NULL;
     struct list_node_s *temp_p;
 
     // Traverse the list to find the correct position to insert
-    while (curr_p != NULL && curr_p->data < value)
-    {
+    while (curr_p != NULL && curr_p->data < value) {
         pred_p = curr_p;
         curr_p = curr_p->next;
     }
 
     // If value is not already in the list, insert the new node
-    if (curr_p == NULL || curr_p->data > value)
-    {
+    if (curr_p == NULL || curr_p->data > value) {
         temp_p = malloc(sizeof(struct list_node_s));
         temp_p->data = value;
         temp_p->next = curr_p;
 
-        if (pred_p == NULL)
-        {
+        if (pred_p == NULL) {
             // New first node
             *head_pp = temp_p;
-        }
-        else
-        {
+        } else {
             pred_p->next = temp_p;
         }
         pthread_mutex_unlock(&list_mutex);
         return 1;
-    }
-    else
-    {
+    } else {
         // Value already in list
         pthread_mutex_unlock(&list_mutex);
         return 0;
@@ -130,40 +112,31 @@ int Insert(int value, struct list_node_s **head_pp)
 }
 
 // Function to delete a node from the list
-int Delete(int value, struct list_node_s **head_pp)
-{
+int Delete(int value, struct list_node_s **head_pp) {
     pthread_mutex_lock(&list_mutex);
-    count_d++;
 
     struct list_node_s *curr_p = *head_pp;
     struct list_node_s *pred_p = NULL;
 
     // Traverse the list to find the node to delete
-    while (curr_p != NULL && curr_p->data < value)
-    {
+    while (curr_p != NULL && curr_p->data < value) {
         pred_p = curr_p;
         curr_p = curr_p->next;
     }
 
     // If the node is found, delete it
-    if (curr_p != NULL && curr_p->data == value)
-    {
-        if (pred_p == NULL)
-        {
+    if (curr_p != NULL && curr_p->data == value) {
+        if (pred_p == NULL) {
             // Deleting the first node in the list
             *head_pp = curr_p->next;
             free(curr_p);
-        }
-        else
-        {
+        } else {
             pred_p->next = curr_p->next;
             free(curr_p);
         }
         pthread_mutex_unlock(&list_mutex);
         return 1;
-    }
-    else
-    {
+    } else {
         // Value isn't in list
         pthread_mutex_unlock(&list_mutex);
         return 0;
@@ -171,55 +144,50 @@ int Delete(int value, struct list_node_s **head_pp)
 }
 
 // Function executed by each thread
-void *ThreadFunction(void *arg)
-{
-    struct list_node_s **head_pp = (struct list_node_s **)arg;
-    pthread_mutex_lock(&list_mutex1);
-    int i = count++;
-    pthread_mutex_unlock(&list_mutex1);
-    while (i < m)
-    {
-        // only one thread can get access to the linked-list at a time
-        if (count_m < m_member)
-        {
-            printf("Looking for %d: ", opr_values[i]);
-            printf("%d\n", Member(opr_values[i], *head_pp));
-        }
-        else if (count_i < m_insert)
-        {
-            printf("Inserting %d\n", opr_values[i]);
+void *ThreadFunction(void *arg) {
+    thread_data_t *data = (thread_data_t *)arg;
+    struct list_node_s **head_pp = data->head_pp;
+    long rank = data->rank;
+
+    int start = rank;
+    int step = thread_count;
+
+    for (int i = start; i < m; i += step) {
+        if (i < m_member) {
+            printf("Thread %ld: Looking for %d: %d\n", rank, opr_values[i], Member(opr_values[i], *head_pp));
+        } else if (i < m_member + m_insert) {
+            printf("Thread %ld: Inserting %d\n", rank, opr_values[i]);
             Insert(opr_values[i], head_pp);
-        }
-        else
-        {
-            printf("Deleting %d\n", opr_values[i]);
+        } else {
+            printf("Thread %ld: Deleting %d\n", rank, opr_values[i]);
             Delete(opr_values[i], head_pp);
         }
-        pthread_mutex_lock(&list_mutex1);
-        i = count++;
-        pthread_mutex_unlock(&list_mutex1);
     }
-    // Example operations
-    // Insert(3, head_pp);
-    // Insert(1, head_pp);
-    // Insert(2, head_pp);
-    // printf("Is 2 in the list? %d\n", Member(2, *head_pp)); // Output: 1
-    // printf("Is 4 in the list? %d\n", Member(4, *head_pp)); // Output: 0
-    // Delete(2, head_pp);
-    // printf("Is 2 in the list after deletion? %d\n", Member(2, *head_pp)); // Output: 0
-
     return NULL;
 }
 
-int main(int argc, char *argv[])
-{
+void print_list(struct list_node_s *head) {
+    struct list_node_s *current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s <thread_count> <m_member_ratio> <m_insert_ratio> <m_delete_ratio>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     thread_count = strtol(argv[1], NULL, 10);
     n = 1000;
     m = 10000;
-    m_member = m * (float)atof(argv[2]);
-    m_insert = m * (float)atof(argv[3]);
-    m_delete = m * (float)atof(argv[4]);
-    printf(" %d %d %d %d %d\n", n, m, m_member, m_insert, m_delete);
+    m_member = m * atof(argv[2]);
+    m_insert = m * atof(argv[3]);
+    m_delete = m * atof(argv[4]);
+    printf("%d %d %d %d %d\n", n, m, m_member, m_insert, m_delete);
 
     opr_values = malloc(m * sizeof(int));
     int i, ins_value;
@@ -227,13 +195,13 @@ int main(int argc, char *argv[])
     // Create linked list head pointer
     struct list_node_s *head = NULL;
 
-    for (i = 0; i < n; i++)
-    {
+    srand(time(NULL)); // Set different random seed for each execution
+
+    for (i = 0; i < n; i++) {
         ins_value = rand() % 65535; // value should be between 2^16 - 1
         InsertWithoutParellel(ins_value, &head);
     }
-    for (i = 0; i < m; i++)
-    {
+    for (i = 0; i < m; i++) {
         opr_values[i] = rand() % 65535; // value should be between 2^16 - 1
     }
 
@@ -241,51 +209,51 @@ int main(int argc, char *argv[])
 
     // Initialize mutex
     pthread_mutex_init(&list_mutex, NULL);
-    pthread_mutex_init(&list_mutex1, NULL);
 
     // Create threads
-    // pthread_t thread1;
-    // pthread_create(&thread1, NULL, ThreadFunction, (void *)&head);
-    // pthread_create(&thread2, NULL, ThreadFunction, (void *)&head);
-
-    // Wait for threads to finish
-    // pthread_join(thread1, NULL);
-    // pthread_join(thread2, NULL);
-
-    pthread_t *thread_handles;
-    thread_handles = malloc(thread_count * sizeof(pthread_t));
-    long thread;
+    pthread_t *thread_handles = malloc(thread_count * sizeof(pthread_t));
+    thread_data_t *thread_data_array = malloc(thread_count * sizeof(thread_data_t));
 
     clock_t start, end;
     double cpu_time_used;
     start = clock();
-    for (thread = 0; thread < thread_count; thread++)
-    {
+    for (long thread = 0; thread < thread_count; thread++) {
+        thread_data_array[thread].head_pp = &head;
+        thread_data_array[thread].rank = thread;
         printf("Creating thread %ld\n", thread);
-        pthread_create(&thread_handles[thread], NULL, ThreadFunction, (void *)&head);
+        pthread_create(&thread_handles[thread], NULL, ThreadFunction, (void *)&thread_data_array[thread]);
     }
-    for (thread = 0; thread < thread_count; thread++)
-    {
+    for (long thread = 0; thread < thread_count; thread++) {
         pthread_join(thread_handles[thread], NULL);
     }
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("Time taken: %f\n", cpu_time_used);
 
+    // Save the results to a file
+    FILE *file = fopen("results.txt", "a");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file for writing\n");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(file, "n: %d, m: %d, m_member: %d, m_insert: %d, m_delete: %d, thread_count: %d, time: %f\n", n, m, m_member, m_insert, m_delete, thread_count, cpu_time_used);
+    fclose(file);
+
     // Destroy mutex
     pthread_mutex_destroy(&list_mutex);
-    pthread_mutex_destroy(&list_mutex1);
     free(thread_handles);
+    free(thread_data_array);
 
     // Free the allocated memory
     struct list_node_s *current = head;
     struct list_node_s *next;
-    while (current != NULL)
-    {
+    while (current != NULL) {
         next = current->next;
         free(current);
         current = next;
     }
+
+    free(opr_values);
 
     return 0;
 }
